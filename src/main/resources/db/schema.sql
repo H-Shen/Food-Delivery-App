767a4aba0c9f3ce836ea7b08e620ce2396617eae
Haohu Shen <haohu3991@gmail.com>
2022-02-28 07:45:04 -0700

    1st commit

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE t_user
     `address`   VARCHAR(256)       NULL,
     `create_at` DATETIME           NULL,
     `update_at` DATETIME           NULL,
-    `role`      INT                NOT NULL DEFAULT 0, /** 0,1 **/
+    `role`      INT                NOT NULL DEFAULT 0, /** 0,1,2 **/
     PRIMARY KEY (`id`),
     UNIQUE KEY `phone` (`phone`)
 ) ENGINE = InnoDB
@@ -21,10 +21,10 @@ CREATE TABLE t_user
 DROP TABLE IF EXISTS t_product;
 CREATE TABLE t_product
 (
-    `id`    INT AUTO_INCREMENT NOT NULL,
-    `name`  VARCHAR(64)        NULL,
-    `price` DECIMAL            NULL,
-    `percentage_off`  INT      NULL,
+    `id`          INT AUTO_INCREMENT NOT NULL,
+    `name`        VARCHAR(64)        NULL,
+    `price`       DECIMAL            NULL,
+    `percent_off` INT                NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `name` (`name`)
 ) ENGINE = InnoDB
@@ -34,41 +34,41 @@ CREATE TABLE t_product
 DROP TABLE IF EXISTS t_merchant_product;
 CREATE TABLE t_merchant_product
 (
-    `merchant_id`   INT NOT NULL,
-    `product_id`    INT NOT NULL,
-    PRIMARY KEY (`merchant_id`,`product_id`)
-)  ENGINE = InnoDB
-   DEFAULT CHARSET = utf8mb4
-   COLLATE = utf8mb4_general_ci COMMENT ='商家商品表';
+    `merchant_id` INT NOT NULL,
+    `product_id`  INT NOT NULL,
+    PRIMARY KEY (`merchant_id`, `product_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci COMMENT ='商家商品表';
 
 DROP TABLE IF EXISTS t_shopping_cart;
 CREATE TABLE t_shopping_cart
 (
-    `user_id`   INT NOT NULL,
+    `user_id`    INT NOT NULL,
     `product_id` INT NOT NULL,
     `quantity`   INT NULL,
     PRIMARY KEY (`user_id`, `product_id`)
-)  ENGINE = InnoDB
-   DEFAULT CHARSET = utf8mb4
-   COLLATE = utf8mb4_general_ci COMMENT ='购物车表';
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci COMMENT ='购物车表';
 
 DROP TABLE IF EXISTS t_order;
 CREATE TABLE t_order
 (
-    `order_id` LONG NOT NULL,   /** 雪花算法生成 **/
-    `buyer_id` INT NOT NULL,
-    `merchant_id` INT NOT NULL,
-    `address` VARCHAR(256) NULL,
-    `phone` VARCHAR(20) NULL,
-    `payment_method` INT NULL,  /** 0 1 2 **/
-    `delivery_time` DATETIME NULL,
-    `delivery_method` INT NULL, /** 0 1 2 **/
-    `total_price`  DECIMAL NULL,
-    `delivery_fee` DECIMAL NULL,
-    `tax` DECIMAL NULL,
-    `tip` DECIMAL NULL,
-    `comment` VARCHAR(512) NULL,
-    `create_at` DATETIME NULL
+    `order_id`        INT AUTO_INCREMENT NOT NULL,
+    `buyer_id`        INT          NOT NULL,
+    `merchant_id`     INT          NOT NULL,
+    `address`         VARCHAR(256) NULL,
+    `phone`           VARCHAR(20)  NULL,
+    `payment_method`  INT          NULL, /** 0 1 2 **/
+    `delivery_time`   DATETIME     NULL,
+    `delivery_method` INT          NULL, /** 0 1 2 **/
+    `total_price`     DECIMAL      NULL,
+    `delivery_fee`    DECIMAL      NULL,
+    `tax`             DECIMAL      NULL,
+    `tip`             DECIMAL      NULL,
+    `comment`         VARCHAR(512) NULL,
+    `create_at`       DATETIME     NULL
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_general_ci COMMENT ='订单表';
